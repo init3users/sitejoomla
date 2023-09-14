d7999f2ff7f1e39d558e99aa94bddffc24868afa
dinesh <dineshdh1418@gmail.com>
2023-09-14 05:44:58 +0000

    site1

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-09-14 05:37:01 UTC
+#Software: Joomla! 4.0.3 Stable [ Furaha ] 12-September-2021 10:39 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-09-14T05:37:01+00:00	INFO 192.168.0.101	joomlafailure	Empty password not allowed.
+2023-09-14T05:37:03+00:00	INFO 192.168.0.101	joomlafailure	Empty password not allowed.
+2023-09-14T05:38:08+00:00	INFO 192.168.0.101	joomlafailure	Username and password do not match or you do not have an account yet.
